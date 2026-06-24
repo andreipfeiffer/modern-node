b8b8b3738d0e635b7b8dfca8fab176137b0a3082
Andrei Pfeiffer <andrei@upsidedown.ro>
2026-06-24 08:45:31 +0300

    Reset files

--- src/uuid.js
+++ src/uuid.js
@@ -1,7 +1,7 @@
-// import * as uuid from "uuid";
-import { randomUUID } from "node:crypto";
+import * as uuid from "uuid";
+// import { randomUUID } from "node:crypto";
 
-// const uid = uuid.v4();
-const uid = randomUUID();
+const uid = uuid.v4();
+// const uid = randomUUID();
 
 console.log({ uid });
